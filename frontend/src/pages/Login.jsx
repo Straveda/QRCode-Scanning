38f7ad6356e94a36d8baa7aa05a68e480ddf9320
Aarush Kashyap <aarushk1511@gmail.com>
2025-11-15 21:12:08 +0530

    Fixed second frontend typo

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/login.css";
+import "../styles/Login.css";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
